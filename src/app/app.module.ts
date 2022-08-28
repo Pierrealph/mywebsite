ee4bf366359704b420cf4d39c0c2c271658bbd32
Pierre Alphonse <pierre.alphonse@hotmail.com>
2022-08-28 22:22:53 +0200

    Begin landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {CoreModule} from "./core/core.module";
+import { CoreModule } from "./core/core.module";
+import { LandingPageModule } from "./landing-page/landing-page.module";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import {CoreModule} from "./core/core.module";
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    LandingPageModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
